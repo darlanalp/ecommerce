2c412bcacff8cd386813f1c6a7617e1cb9f8d9e1
darlanalp <darlanalp@gmail.com>
2023-11-27 20:19:07 -0300

    feat(home) :Adicionado o component home

--- ecommerce/modules/feature/home/src/index.ts
+++ ecommerce/modules/feature/home/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/home.module';
 export * from './lib/lib.routes';
 
 export * from './lib/lib.routes';
+
+export * from './lib/home/home.component';
--- ecommerce/modules/feature/home/src/lib/home.module.ts
+++ ecommerce/modules/feature/home/src/lib/home.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { homeRoutes } from './lib.routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(homeRoutes)],
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
 })
 export class HomeModule {}
