cfa85e1a5fd1bc2f15a3c9575a672c76ee878a07
darlanalp <darlanalp@gmail.com>
2023-11-27 21:01:00 -0300

    feat(layout): criando o limk  layzload no home

--- ecommerce/src/app/app.routes.ts
+++ ecommerce/src/app/app.routes.ts
@@ -1,3 +1,14 @@
+
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path: 'home',
+        loadChildren: () => import('home').then((m) => m.HomeModule),
+    },
+];
