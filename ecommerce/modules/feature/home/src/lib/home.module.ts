8153508a8d43c48d39590b5eca77e8b69fda70b7
Darlan Alvese <darlanalp@gmail.comm>
2023-12-18 21:17:34 -0300

    feat(home): add recomendação de produtos

--- ecommerce/modules/feature/home/src/lib/home.module.ts
+++ ecommerce/modules/feature/home/src/lib/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { homeRoutes } from './lib.routes';
 import { HomeComponent } from './home/home.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(homeRoutes)],
+  imports: [CommonModule, RouterModule.forChild(homeRoutes), MatCardModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
 })
-export class HomeModule {}
+export class HomeModule { }
