2048948a72f822d284c3d46108a984fb0de2f773
Darlan Alvese <darlanalp@gmail.comm>
2024-01-08 14:56:44 -0300

    feat(product-detail): adicionando rota de detalhe de produto

--- ecommerce/modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
+++ ecommerce/modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -1,5 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, map } from 'rxjs';
+
+/**
+ * Este exemplo é para demonstrar que podemos criar funcções separadas 
+ * utilizando o inject, que é uma forma alternativa de injeção de depedência.
+ * Pois se não usar esse cara, deveria ser criado no construtor da classe um objeto e depois utilizar,
+ * e com o inject é possivel isolar regras deixando mais reaproveitavel o código
+ * @returns 
+ */
+function getParamsId(): Observable<string> {
+
+  //vamos utilizar o pipe para usar o pipe async no html, exemplo: {{id$ | async}}, desta forma não precisa
+  //fazer o subscriber, evitando assim o vazamento de memória.
+  return inject(ActivatedRoute).params.pipe(map((params) => params['id']));
+}
 
 @Component({
   selector: 'lib-product-detail',
@@ -8,4 +24,8 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
 })
-export class ProductDetailComponent {}
+
+export class ProductDetailComponent {
+  constructor() { }
+  id$ = getParamsId();
+}
